72bfc3e79476c0c05c57ab264aa98bc018f23fff
Kyle Henson <khenson.kh@gmail.com>
2015-04-16 16:58:48 -0600

    Generate phone number and associate with person

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Person, type: :model do
   it 'is valid' do
     expect(person).to be_valid
   end
-  
+
   it 'is invalid without a first name' do
     person.first_name = nil
     expect(person).not_to be_valid
@@ -17,5 +17,9 @@ RSpec.describe Person, type: :model do
   it 'is invalid without a last name' do
     person.last_name = nil
     expect(person).not_to be_valid
+  end
+
+  it 'has an array of phone numbers' do
+    expect(person.phone_numbers).to eq([])
   end
 end
