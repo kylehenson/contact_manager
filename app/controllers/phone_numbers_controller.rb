90908cda26c4ea7bb9dd37367784bd1ba49b3e8a
Kyle Henson <khenson.kh@gmail.com>
2015-04-19 14:26:31 -0600

    Finish implementing phone number functionality

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -32,7 +32,7 @@ class PhoneNumbersController < ApplicationController
   def update
     respond_to do |format|
       if @phone_number.update(phone_number_params)
-        format.html { redirect_to @phone_number, notice: 'Phone number was successfully updated.' }
+        format.html { redirect_to @phone_number.person, notice: 'Phone number was successfully updated.' }
         format.json { render :show, status: :ok, location: @phone_number }
       else
         format.html { render :edit }
@@ -41,23 +41,19 @@ class PhoneNumbersController < ApplicationController
     end
   end
 
-  # DELETE /phone_numbers/1
-  # DELETE /phone_numbers/1.json
   def destroy
     @phone_number.destroy
     respond_to do |format|
-      format.html { redirect_to phone_numbers_url, notice: 'Phone number was successfully destroyed.' }
+      format.html { redirect_to @phone_number.person, notice: 'Phone number was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_phone_number
       @phone_number = PhoneNumber.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def phone_number_params
       params.require(:phone_number).permit(:number, :person_id)
     end
